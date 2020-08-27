1fadfb1696cf2edf2e4125bafa9866dd9a75eb62
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-08-27 19:44:32 -0300

    exercicio 11 js primeiros passos

--- modulo_intro_web/bloco_4/dia_1/exercicios/exercicio10.js
+++ modulo_intro_web/bloco_4/dia_1/exercicios/exercicio10.js
@@ -11,10 +11,10 @@ let custoComTaxa = (custoProduto * 0.2) + custoProduto; // 0.2 equivale aos 20%.
 if (custoProduto > 0 && valorVenda >0){
   // O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
   let total = ((valorVenda - custoComTaxa) * 1000);
-  console.log('Valor do lucro é: ' + total)  
+  console.log('Valor do lucro é: ' + total); 
 }else{
   // Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
-  console.log('Valores inválidos')
-}
+  console.log('Valores inválidos');
+};
 
 // console.log((valorVenda - custoProduto) * 1000);
