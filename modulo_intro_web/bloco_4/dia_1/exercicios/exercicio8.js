41fc5fbaf6e5e8d995ba084d377ce3f75afabdc1
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-08-27 18:33:38 -0300

    exercicio 9 js primeiros passos

--- modulo_intro_web/bloco_4/dia_1/exercicios/exercicio8.js
+++ modulo_intro_web/bloco_4/dia_1/exercicios/exercicio8.js
@@ -2,7 +2,7 @@ let n1 = 1;
 let n2 = 5;
 let n3 = 9;
 
-if (n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 00){
+if (n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 0){
   console.log('true');
 }else{
   console.log('false');
