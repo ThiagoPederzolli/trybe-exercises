dbfa06ed459f79f269fd207ab0875243e62aa523
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-08-31 18:46:44 -0300

    exercicio 2 dia 3 bloco 4

---
+++ modulo_intro_web/bloco_4/dia_3/exercicio02.js
@@ -0,0 +1,8 @@
+let n = 5;
+let linhas = '';
+
+//para cada loop adiciona um asterisco a string linhas e já imprime ela no console, ou seja, a cada loop é impressa uma string com um asterisco a mais que a anterior.
+for (i = 0; i < n; i += 1){
+  linhas += '*';
+  console.log(linhas);
+}
