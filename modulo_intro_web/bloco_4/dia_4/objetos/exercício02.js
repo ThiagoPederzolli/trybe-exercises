aa33e685fc51efa6c0407c7966695668029cffd7
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-01 18:26:35 -0300

    exercicio 4 funcoes dia4 bloco4

--- modulo_intro_web/bloco_4/dia_4/objetos/exercício02.js
+++ modulo_intro_web/bloco_4/dia_4/objetos/exercício02.js
@@ -2,7 +2,7 @@ let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim'
+  recorçente: 'Sim'
 };
 
-console.log(info.recorrente);
+console.log(info.recorçente);
