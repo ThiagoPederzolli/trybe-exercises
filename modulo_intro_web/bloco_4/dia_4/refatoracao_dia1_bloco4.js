f3fdcf42d617450915a7677b2437969977580d37
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-01 13:33:33 -0300

    refatoração dia 1 bloco 4 exercicio 1

---
+++ modulo_intro_web/bloco_4/dia_4/refatoracao_dia1_bloco4.js
@@ -0,0 +1,20 @@
+function sum(a, b){
+  return a + b;
+}
+
+function sub(a, b){
+  return a - b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function multiple(a, b){
+  return a * b;
+}
+
+console.log(sum(4, 8));
+console.log(sub(8, 5));
+console.log(divide(16, 4));
+console.log(multiple(8, 2));
