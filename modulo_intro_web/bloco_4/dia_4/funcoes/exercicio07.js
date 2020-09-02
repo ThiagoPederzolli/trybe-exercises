26a13134e32002d1e676f0dc85ab59624b35aa15
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-02 12:39:15 -0300

    correcao exercicio7 funcoes dia4 bloco4

--- modulo_intro_web/bloco_4/dia_4/funcoes/exercicio07.js
+++ modulo_intro_web/bloco_4/dia_4/funcoes/exercicio07.js
@@ -1,9 +1,45 @@
-function palavra(ending, word){
-  if(ending.includes(word)){
-    return true
-  } else {
-    return false
+function palavra(word, ending){
+  let end = '';
+  let endword = '';
+  for(i = 1; i < ending.length + 1; i += 1){    
+    if(word[word.length - i] == ending[ending.length - i]){
+      end += ending[ending.length - i];
+      endword += word[word.length - i];
+    }else{
+      return false;
+    }
   }
+  if (end === endword && end != ''){
+    return true;
+  }else{
+    return false;
+  }
+
 }
 
-console.log(palavra('coca-cola', 'bola'));
+console.log(palavra('javierfernando', 'javier'));
+
+
+
+
+
+
+
+// function palavra(word, ending){
+//   if(word.indexOf(ending) > -1){
+//     return true
+//   }else{
+//     return false
+//   }
+// }
+
+
+
+// // function palavra(ending, word){
+// //   if(ending.includes(word)){
+// //     return true
+// //   } else {
+// //     return false
+// //   }
+// // }
+
