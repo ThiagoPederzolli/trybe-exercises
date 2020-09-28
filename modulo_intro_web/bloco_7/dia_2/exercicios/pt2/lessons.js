59b3c5f51917aadb96ada24d3046cbb0e5ce78da
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-28 12:23:55 -0300

    exercicios pt2 requisito 1 dia 2 bloco 7

---
+++ modulo_intro_web/bloco_7/dia_2/exercicios/pt2/lessons.js
@@ -0,0 +1,31 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const lesson5 = {
+  materia: 'Português',
+  numeroEstudantes: 10,
+  professor: 'Pedro',
+  turno: 'noite',
+};
+
+const incrementKey = (objeto, chave, valor) => objeto[`${chave}`] = valor;
+
+incrementKey(lesson5, 'grau', 'médio');
+console.log(lesson5);
