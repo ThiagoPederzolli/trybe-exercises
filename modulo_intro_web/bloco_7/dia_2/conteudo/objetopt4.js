dcd78503ab677b28c7fd38459e30e8874705f223
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-28 11:32:32 -0300

    conteudo pt4 dia 2 bloco 7

---
+++ modulo_intro_web/bloco_7/dia_2/conteudo/objetopt4.js
@@ -0,0 +1,8 @@
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(Object.entries(países)[0][0]);
