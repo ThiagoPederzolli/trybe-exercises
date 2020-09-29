beda53aa540173aa42c478b4d34c6b87eacf2298
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-29 12:50:54 -0300

    exercicio TDD wordLengths dia 3 bloco 7

--- modulo_intro_web/bloco_7/dia_3/exercicios/TDD/wordLengths.js
+++ modulo_intro_web/bloco_7/dia_3/exercicios/TDD/wordLengths.js
@@ -1,9 +1,15 @@
 const assert = require('assert');
 // escreva a função wordLengths aqui
-
+function wordLengths(array) {
+  let lengths = []
+  for (let i = 0; i < array.length; i += 1) {
+    lengths.push(array[i].length);
+  }
+  return lengths;
+}
 const words = ['sun', 'potato', 'roundabout', 'pizza'];
 const expected = [3, 6, 10, 5];
 
-assert.strictEqual(typeof wordLengths, 'function');
+// assert.strictEqual(typeof wordLengths, 'function');
 const output = wordLengths(words);
-assert.deepEqual(output, expected);
+assert.deepStrictEqual(output, expected);
