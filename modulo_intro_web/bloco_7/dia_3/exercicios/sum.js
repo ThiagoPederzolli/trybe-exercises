4ff3922b18fafca2447bca4956c7ed9c2b7ce27b
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-29 11:28:25 -0300

    exercicio sum req 1 e 2 dia 3 bloco 7

---
+++ modulo_intro_web/bloco_7/dia_3/exercicios/sum.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+// assert(sum(4, 5) === 9);
+assert(sum(0,0) === 0);
