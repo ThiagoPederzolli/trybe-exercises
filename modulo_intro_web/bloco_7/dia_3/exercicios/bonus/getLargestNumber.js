ed2590ac3a4b486b5dc53b57c7790166a9e17dc9
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-29 14:25:33 -0300

    exercicio bonus 5 dia 3 bloco 7

---
+++ modulo_intro_web/bloco_7/dia_3/exercicios/bonus/getLargestNumber.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+const getLargestNumber = (array) => {
+  array.sort((a, b)=>b-a)
+  return array[0];
+}
+
+const parameter = [45, 8, 2, 50, 1, 7, 99];
+const result = 99;
+const output = getLargestNumber(parameter);
+
+assert.strictEqual(output, result);
