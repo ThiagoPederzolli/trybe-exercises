4fd04d355b6104638d812ca5f415eece50ba2386
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-05 14:34:00 -0300

    conteudo dia 1 bloco 8

---
+++ modulo_intro_web/bloco_8/conteudo/some.js
@@ -0,0 +1,8 @@
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => {
+  return names.some((name) => name[0] === letter);
+};
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('x', listNames)); // false
