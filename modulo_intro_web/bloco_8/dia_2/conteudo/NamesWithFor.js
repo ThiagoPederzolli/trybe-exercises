6205383670e85d72c92e5c69b8b4d5d495a1e7fd
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-06 15:13:50 -0300

    conteudo sobre map e exericio 1 dia 2 bloco8

---
+++ modulo_intro_web/bloco_8/dia_2/conteudo/NamesWithFor.js
@@ -0,0 +1,13 @@
+const persons = [
+  { firstName: "Maria", lastName: "Ferreira" },
+  { firstName: "João", lastName: "Silva" },
+  { firstName: "Antonio", lastName: "Cabral" },
+];
+
+const fullNames = [];
+for(let i = 0; i < persons.length; i += 1){
+  fullNames.push(`${persons[i].firstName} ${persons[i].lastName}`);
+}
+
+console.log(fullNames);
+
