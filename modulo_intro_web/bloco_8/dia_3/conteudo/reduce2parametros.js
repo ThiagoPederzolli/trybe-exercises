9cd980af89aab4652ac1dfd5b1d2ca9ae30d3578
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-07 15:07:02 -0300

    conteudo 1 e 2 dia 3 bloco8

---
+++ modulo_intro_web/bloco_8/dia_3/conteudo/reduce2parametros.js
@@ -0,0 +1,8 @@
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 10 42 57 60 62 57 113
+  return result + number;
+  };
+const sumNumbers = numbers.reduce(getSum, 10);
+console.log(sumNumbers); // 123
