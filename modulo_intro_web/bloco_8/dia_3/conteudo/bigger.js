64930972d5f8e0b8d0c78a97dedfd33431295901
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-07 15:16:01 -0300

    somando pares conteudo dia 3 bloco 8

---
+++ modulo_intro_web/bloco_8/dia_3/conteudo/bigger.js
@@ -0,0 +1,6 @@
+const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => (bigger > number) ? bigger : number;
+
+const bigger = numbers.reduce(getBigger, 100);
+console.log(bigger); // 85
