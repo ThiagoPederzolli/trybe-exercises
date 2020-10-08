c33e7fc15183ea1dd52d213b1eda0a31c95ebd20
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-08 14:01:53 -0300

    conteudo abreviation object literal

---
+++ modulo_intro_web/bloco_8/dia_4/conteudo/abreviationObjectLiteral.js
@@ -0,0 +1,15 @@
+
+// Use object property shorthand with object literals to create and return an object with name, age and gender properties.
+// createPerson("Zodiac Hasbro", 56, "male") should return {name: "Zodiac Hasbro", age: 56, gender: "male"}.
+// Your code should not use key:value.
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // Only change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // Only change code above this line
+};
