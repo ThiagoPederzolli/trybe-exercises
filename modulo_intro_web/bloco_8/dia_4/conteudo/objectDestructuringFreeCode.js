b45ebde779b3dbf54ce70f007bd4edd065fe8876
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-08 13:25:38 -0300

    conteudo object destructuring

---
+++ modulo_intro_web/bloco_8/dia_4/conteudo/objectDestructuringFreeCode.js
@@ -0,0 +1,18 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
+
+
+// Only change code above this line
+
+// Only change code below this line
+  
+const {today:highToday, tomorrow:highTomorrow} = HIGH_TEMPERATURES;
+
+// Only change code above this line
