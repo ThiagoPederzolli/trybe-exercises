36e462b31782f7be7d3ef11f9e089e726f060b68
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-08 14:50:20 -0300

    conteudos e exercicio 1 dia 4 bloco8

--- modulo_intro_web/bloco_8/dia_4/conteudo/defaultParams.js
+++ modulo_intro_web/bloco_8/dia_4/conteudo/defaultParams.js
@@ -11,3 +11,14 @@ say(undefined, undefined);
 // > Hello People
 // > Hi World
 // > Hello World
+
+// Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified.
+// The result of increment(5, 2) should be 7.
+// The result of increment(5) should be 6.
+// A default parameter value of 1 should be used for value.
+
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+
