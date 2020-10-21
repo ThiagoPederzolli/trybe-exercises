b9106043e6c07e3100cfa78ce70a3588df4eeae4
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-21 18:05:18 -0300

    exercicio fixacao lidando com erros

---
+++ modulo_intro_web/bloco_9/operacoes-assincronas/lidando-erros.js
@@ -0,0 +1,31 @@
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onFail) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    }
+    else {
+      const errorMessage = "Country could not be found";
+      onFail(errorMessage);
+    }
+  }, delay());
+  return ''
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+// console.log(getCountry(countryName, printErrorMessage));
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+console.log(getCountry(countryCurrency, printErrorMessage));
