8eef8edef5cc54c9956acd001b3184dce95a148d
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-21 18:56:46 -0300

    exemplificacao fluxo callback fixacao1 dia1 bloco9

--- modulo_intro_web/bloco_9/callback/exercicio-fixacao1.js
+++ modulo_intro_web/bloco_9/callback/exercicio-fixacao1.js
@@ -14,3 +14,27 @@ const getUser = (callback) => {
 
 assert.equal(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
 assert.equal(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
+
+
+
+// function getUser(callback) {
+//   const userToReturn = {
+//     firstName: "Ivan",
+//     lastName: "Ivanovich",
+//     nationality: "Russian"
+//   };
+//   return callback(userToReturn);
+// }
+
+// getUser(userFullName) // chamei a função getUser e passei o corpo de userFullName como parâmetro:
+
+// function getUser(userFullName) {
+//   const userToReturn = {
+//     firstName: "Ivan",
+//     lastName: "Ivanovich",
+//     nationality: "Russian"
+//   };
+//   return userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// }
+
+// // Por isso getUser(userFullName) retorna "Hello! My name is Ivan Ivanovich"
