15eee9fb77dd73e883c417ea3dd56a4b9eb786a3
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-22 16:07:48 -0300

    transformando promise em async

--- modulo_intro_web/bloco_9/dia_2/fetch_API/fetch2.js
+++ modulo_intro_web/bloco_9/dia_2/fetch_API/fetch2.js
@@ -3,9 +3,10 @@ const fetch = require('node-fetch');
 function verifiedFetch(url) {
   return new Promise((resolve, reject) => {
     if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
-      fetch(url)
-        .then((r) => r.json())
-        .then((r) => resolve(r.value));
+      // fetch(url)
+      //   .then((r) => r.json())
+      //   .then((r) => resolve(r.value));
+      return resolve('testando retorno');
     } else {
       reject(new Error('endpoint não existe'));
     }
