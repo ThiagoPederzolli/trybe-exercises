4a3f7cc27f18293c4d520b1fb27626cf365263b7
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-22 18:08:54 -0300

    transformando promise em async await

--- modulo_intro_web/bloco_9/dia_2/fetch_API/async.js
+++ modulo_intro_web/bloco_9/dia_2/fetch_API/async.js
@@ -4,13 +4,13 @@ async function verifiedFetch(url) {
   if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
     return fetch(url)
       .then((r) => r.json())
-      .then((r) => (r.value));
+      .then((r) => r.value);
   }
   throw new Error('endpoint não existe');
 }
 
-async function sendJokeToFriend(name) {
-  const message = await verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+function sendJokeToFriend(name) {
+  const message = verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
     .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
     .catch((err) => err);
   console.log(message);
