b73c1874f231cb0eb4c34504f02048ed02549d0b
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-25 15:56:56 -0300

    correcao exercicio 2 e resolucao exericio 3

--- modulo_intro_web/bloco_10/jest/exercicios_dia1/myRemoveWithoutCopy.test.js
+++ modulo_intro_web/bloco_10/jest/exercicios_dia1/myRemoveWithoutCopy.test.js
@@ -1,21 +1,20 @@
-const myRemove = require('./myRemove');
+const myRemoveWithoutCopy = require('./myRemoveWithoutCopy');
 
-describe('A função myRemove', () => {
+describe('A função myRemoveWithoutCopy', () => {
   it('Deve retornar um array sem conter o elemento declarado no segundo argumento', () => {
     const arg = [1, 2, 3, 4];
-    expect(myRemove(arg, 3)).not.toContain(3);
+    expect(myRemoveWithoutCopy(arg, 3)).not.toContain(3);
   });
   it('Deve ter o retorno diferente do array passado como argumento', () => {
     const arg = [1, 2, 3, 4];
-    expect(myRemove(arg, 3)).not.toEqual(arg);
+    expect(myRemoveWithoutCopy(arg, 3)).not.toEqual([1, 2, 3, 4]);
   });
   it('O array passado por argumento deve permanecer o mesmo', () => {
-    const arg = [1, 2, 3, 4];
-    myRemove(arg, 3)
-    expect(arg).toEqual([1, 2, 3, 4]);
+    const arg = [1, 2, 3, 4];    
+    expect(myRemoveWithoutCopy(arg, 3)).toEqual([1, 2, 4]);
   });
   it('Deve retornar o mesmo array caso o elemento declarado no segundo argumento não pertença ao array', () => {
     const arg = [1, 2, 3, 4];
-    expect(myRemove(arg, 5)).toEqual(arg);
+    expect(myRemoveWithoutCopy(arg, 5)).toEqual(arg);
   });
 })
