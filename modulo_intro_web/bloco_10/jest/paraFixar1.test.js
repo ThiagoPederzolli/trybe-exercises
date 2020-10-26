7dcecfc9af7506b74dfacf1d941f919961d82393
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-26 13:48:31 -0300

    conteudo aula dia 3 bloco 10

--- modulo_intro_web/bloco_10/jest/paraFixar1.test.js
+++ modulo_intro_web/bloco_10/jest/paraFixar1.test.js
@@ -1,9 +1,11 @@
 // const paraFixar1 = require('./paraFixar1');
-const randomNumber = () => Math.floor(Math.random() * 100);// Lógica de gerar o número
+let randomNumber = () => Math.floor(Math.random() * 100);// Lógica de gerar o número
 
 const isDivisible = (number) => (randomNumber() % number) === 0
 
 it('randomNumber é chamada', () => {
-  const randomNumber = jest.fn().mockReturnValue(4);
-  return expect(randomNumber).toHaveBeenCalled();
+  randomNumber = jest.fn().mockReturnValue(4);
+  isDivisible(2);
+  expect(randomNumber).toHaveBeenCalled();
+
 })
