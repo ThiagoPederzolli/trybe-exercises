b4d96b2f30284865aaf809b7464cd93509ef04f1
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-25 23:33:41 -0300

    conteudo promise e async/await dia2 bloco10

---
+++ modulo_intro_web/bloco_10/jest/exercicios_dia1/asyncAwait.test.js
@@ -0,0 +1,57 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalsByType = (type) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const arrayAnimals = Animals.filter((animal) => animal.type === type);
+      if (arrayAnimals.length !== 0) {
+        return resolve(arrayAnimals);
+      }
+
+      return reject({ error: 'Não possui esse tipo de animal.' });
+    }, 100);
+  })
+);
+
+const getListAnimals = (type) => (
+  findAnimalsByType(type).then(list => list)
+);
+
+test('Testando com async/await', async () => {
+  const listDogs = await getListAnimals('Dog');
+  expect(listDogs[0].name).toEqual('Dorminhoco');
+  expect(listDogs[1].name).toEqual('Soneca');
+});
+
+test('Testando com async/await, testando o reject', async () => {
+  try {
+    await getListAnimals('Lion');
+  } catch (error) {
+    expect(error).toEqual({ error: "Não possui esse tipo de animal." })
+  }
+});
+
+// Utilizando .resolves / .rejects
+describe('Testando Async/Await - findAnimalsByType', () => {
+  describe('Quando o tipo do animal existe', () => {
+    test('Retorne a lista de animais', async () => {
+      const listDogs = [
+        { name: 'Dorminhoco', age: 1, type: 'Dog' },
+        { name: 'Soneca', age: 2, type: 'Dog' },
+      ]
+      expect.assertions(1);
+      await expect(getListAnimals('Dog')).resolves.toEqual(listDogs)
+    });
+  });
+
+  describe('Quando o tipo de animal não existe', () => {
+    test('Retorna um erro', async () => {
+      expect.assertions(1);
+      await expect(getListAnimals('Lion')).rejects.toEqual({ error: 'Não possui esse tipo de animal.' })
+    });
+  });
+});
