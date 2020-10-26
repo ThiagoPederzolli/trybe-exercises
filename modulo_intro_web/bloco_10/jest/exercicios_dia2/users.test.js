34e09eaa511c1574a02f347c3ccc978902797c20
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-26 08:47:48 -0300

    realocando alguns arquivos

---
+++ modulo_intro_web/bloco_10/jest/exercicios_dia2/users.test.js
@@ -0,0 +1,21 @@
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+};
+
+const findUserById = (id) => {
+  return new Promise((resolve, reject) => {
+      if (users[id]) {
+        return resolve(users[id]);
+      }
+
+      return reject({ error: 'User with ' + id + ' not found.' });
+  });
+}
+
+const getUserName = (userID) => {
+  return findUserById(userID).then(user => user.name);
+}
+
+describe('getUserName')
+test('Should return')
