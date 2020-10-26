74f2e1670f55f5e2ab5984e9a597537ef1c69981
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-26 15:37:51 -0300

    conteudos dia 3 bloco 10

--- modulo_intro_web/bloco_10/jest/paraFixar1.test.js
+++ modulo_intro_web/bloco_10/jest/paraFixar1.test.js
@@ -3,9 +3,30 @@ let randomNumber = () => Math.floor(Math.random() * 100);// Lógica de gerar o n
 
 const isDivisible = (number) => (randomNumber() % number) === 0
 
-it('randomNumber é chamada', () => {
-  randomNumber = jest.fn().mockReturnValue(4);
-  isDivisible(2);
-  expect(randomNumber).toHaveBeenCalled();
+describe('IsDivisible', () => {
+  beforeEach(() => {
+    randomNumber = jest.fn().mockReturnValueOnce(4).mockReturnValueOnce(8).mockReturnValue(15);
+  });
+//Feito isso, escreva um teste que verifica que a função randomNumber() é chamada quando invocamos a isDivisible().
+  it('calls randomNumber', () => {
+    isDivisible();
+    expect(randomNumber).toHaveBeenCalled(); 
+  });
+//Teste que, quando a randomNumber() retorna um número par e isDivisible() recebe um dois, o retorno é true.
+  it('should return true if randomNumber returns a number pair and the arg of IsDivisible is 2', () => {
+    const pairNumber = isDivisible(2);
+    expect(pairNumber).toBeTruthy();
+  });
+//Simule (mocke) dois valores para a função randomNumber() retornar e um terceiro valor default. Chame a função isDivisible() quatro vezes num mesmo teste e cheque que os retornos são conforme esperado.
+  it('should return true if randomNumber returns number divisible for 5', () => {
+    const firstNumber = isDivisible(5);
+    const secondNumber = isDivisible(5);
+    const thirdNumber = isDivisible(5);
+    const fourthNumber = isDivisible(5);
+    expect(firstNumber).toBeFalsy();
+    expect(secondNumber).toBeFalsy();
+    expect(thirdNumber).toBeTruthy();
+    expect(fourthNumber).toBeTruthy();
+  });
 
 })
