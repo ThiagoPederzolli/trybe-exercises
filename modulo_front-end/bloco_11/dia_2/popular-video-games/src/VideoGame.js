57c8149508aa3ae4534f1b9ad70b75d805b41de4
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-28 14:58:59 -0300

    criacao component VideoGame

---
+++ modulo_front-end/bloco_11/dia_2/popular-video-games/src/VideoGame.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class VideoGame extends Component {
+  render() {
+    return (<span>Eu sou o componente VideoGame</span>)
+  }
+}
+
+export default VideoGame;
