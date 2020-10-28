6652fe1fc8ae9e2542c846719a6edc406124f1f5
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-28 09:52:14 -0300

    criando componente Image projeto fixation

--- modulo_front-end/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
+++ modulo_front-end/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Image from './Image';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
   );
 }
 
