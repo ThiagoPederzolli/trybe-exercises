6b456d4343d96771a67d0ceda767c1c2d0c3e444
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-29 18:03:33 -0300

    usando state para filtrar e exibir 1 pokemon

--- modulo_front-end/bloco_12/my-pokedex/src/Pokedex.js
+++ modulo_front-end/bloco_12/my-pokedex/src/Pokedex.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import Pokemon from './Pokemon';
 import pokemons from './data';
 import './Pokedex.css'
+
 class Pokedex extends Component {
-  render() {    
-    return (<div className="pokedex">   
-      {
-      pokemons.map((pokemon) => {
-        return (
-          <Pokemon key={pokemon.id} pokemon={pokemon} />
-        )
-      })
+  constructor() {
+    super();
+
+    this.state = {
+      posPokemon: 0
     }
+
+    this.showPokemon = this.showPokemon.bind(this);
+  }
+
+  showPokemon() {
+    this.setState((posAnterior) => ({
+      posPokemon: posAnterior.posPokemon + 1
+      // posPokemon: (state.posPokemon + 1) % numberOfPokemons
+    }))
+  }
+  render() {
+    // const pokemon = pokemons[this.state.posPokemon] 
+    return (<div className="pokedex"> 
+
+      {/* <Pokemon pokemon={pokemon} /> */}
+        {
+          pokemons.filter((pokemon) => pokemons.indexOf(pokemon) === this.state.posPokemon).map((pokemon) => {
+            return (
+              <Pokemon key={pokemon.id} pokemon={pokemon} />  
+            )
+          })
+        }
+        {/* <button onClick={() => this.showPokemon(pokemons.length)}>Próximo</button> */}
+        <button onClick={this.showPokemon}>Próximo</button>
     </div>)
   }
 }
+
+// filter((pokemon) => pokemons.indexOf(pokemon) === this.state.posPokemon )
 
 export default Pokedex;
