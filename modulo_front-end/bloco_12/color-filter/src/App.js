0cdd587d9fbae511f286223e5766f74eb2506e66
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-29 16:25:35 -0300

    usando state para filtrar elementos

--- modulo_front-end/bloco_12/color-filter/src/App.js
+++ modulo_front-end/bloco_12/color-filter/src/App.js
@@ -3,11 +3,37 @@ import colors from './data';
 import { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      numeroDeXablaus: 0,
+      textoDoFilter: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((numeroAnterior, _props) => ({
+      numeroDeXablaus: numeroAnterior.numeroDeXablaus + 1
+    }))
+
+  }
+
+  handleChange(event) {
+    this.setState({textoDoFilter: event.target.value})
+  }
+
+
   render() {
     return (<div>
-      {colors.map((color) => {
+      <div><input type="text"onChange={this.handleChange}/></div>
+      <div>{colors.filter((color) => color.color === this.state.textoDoFilter).map((color) => {
         return (<div key={color.value}>{color.color}</div>)
       })}
+      </div>
+      <button onClick={this.handleClick}>Xablau {this.state.numeroDeXablaus}</button>
       </div>
     );
   }
