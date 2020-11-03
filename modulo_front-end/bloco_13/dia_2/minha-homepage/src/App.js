5638a3e4c944ad37f80e3ff199a11a63190f1762
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-11-03 14:57:37 -0300

    rotas do projeto minha homepage configuradas

--- modulo_front-end/bloco_13/dia_2/minha-homepage/src/App.js
+++ modulo_front-end/bloco_13/dia_2/minha-homepage/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Home from './Home';
-import BrowserRouter from 'react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
+import About from './About';
+import HowTo from './HowTo';
+import Profile from './Profile';
 
 function App() {
   return (
     <BrowserRouter>
-      <Home />
+      <Route path="/about" component={About} />
+      <Route path="/howto" component={HowTo} />
+      <Route path="/profile"><Profile/></Route>
+      <Route exact path="/" component={Home} />
     </BrowserRouter>
   );
 }
--- modulo_front-end/bloco_13/dia_2/minha-homepage/src/Home.js
+++ modulo_front-end/bloco_13/dia_2/minha-homepage/src/Home.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+// import { Route } from 'react-router-dom';
+// import About from './About';
+// import HowTo from './HowTo';
+// import Profile from './Profile';
 
 class Home extends Component {
   render() {
-    return(<h1>Minha HomePage</h1>)
+    return(
+      <div>
+        <h1>Minha HomePage</h1>      
+      </div>
+    )
   }
 }
 
