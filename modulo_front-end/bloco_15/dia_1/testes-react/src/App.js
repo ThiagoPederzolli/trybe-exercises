ffc49436ea1d2e18dfd079d29f108757ea71ffc9
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-11-05 23:45:35 -0300

    aplicando testes em um componente só dia 1 bloco 15

--- modulo_front-end/bloco_15/dia_1/testes-react/src/App.js
+++ modulo_front-end/bloco_15/dia_1/testes-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ValidEmail from './ValidEmail';
 import './App.css';
 
 class App extends Component {
@@ -39,7 +40,7 @@ class App extends Component {
           onClick={() => this.changeSaveEmail(email)}
         />
         <input id="btn-id" type="button" value="Voltar" />
-        <h2 data-testid="id-email-user">{`Valor: ${saveEmail}`}</h2>
+        <ValidEmail email={saveEmail} />
       </div>
     );
   }
