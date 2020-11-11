3f845b0f47a49448b6cbfecb255e264ba0e400f0
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-11-11 09:39:55 -0300

    corrigida exportacao action Redux dia 2 bloco 16

--- modulo_front-end/bloco_16/dia_2/react-com-redux/src/actions/index.js
+++ modulo_front-end/bloco_16/dia_2/react-com-redux/src/actions/index.js
@@ -1,1 +1,4 @@
-export const addAssignment = value => ({ type: ADD_ELEMENT, value });
+const ADD_ELEMENT = 'ADD_ELEMENT';
+const addAssignment = value => ({ type: ADD_ELEMENT, value });
+
+export default addAssignment;
